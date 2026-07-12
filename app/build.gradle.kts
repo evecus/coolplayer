65745e95d1a79c4bfdae55d05105cbb320973490
evecus <jamie55@163.com>
2026-07-12 20:29:57 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,6 @@ dependencies {
 
     // Media3 ExoPlayer + MediaSession
     implementation("androidx.media3:media3-exoplayer:1.4.1")
-    implementation("androidx.media3:media3-exoplayer-hls:1.4.1")
-    implementation("androidx.media3:media3-ui:1.4.1")
     implementation("androidx.media3:media3-common:1.4.1")
     implementation("androidx.media3:media3-session:1.4.1")
 
